4fdeeb1d122a7a3c39301782360b2f24b3594e65
Saba Schock <sababorjsaba@gmail.com>
2018-11-26 18:42:20 -0800

    start configuration

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD4qgKjBbbDGPY9A_fqzW0wwhZZTxlEqcY",
+    authDomain: "timesheet-4deba.firebaseapp.com",
+    databaseURL: "https://timesheet-4deba.firebaseio.com",
+    projectId: "timesheet-4deba",
+    storageBucket: "timesheet-4deba.appspot.com",
+    messagingSenderId: "571309180176"
+  };
+  firebase.initializeApp(config);
